f9aa6ebf3c20a7da3c500d98fec331357d883a95
CypherIgnis <116596423+CypherIgnis@users.noreply.github.com>
2022-11-21 16:33:52 -0300

    Create profileimport.py

---
+++ profileimport.py
@@ -0,0 +1,11 @@
+from claseprofile import Profile
+
+def main():
+    profile=Profile
+    id=input("ingrese el id=")
+    nickname=input("ingrese su nickname=")
+    avatar=input("diseñe su avatar=")
+    status=input("ingrese su estado=")
+    created_at=input("ingrese su fecha de ingreso=")
+    mail=input("ingrese su mail=")
+    is_premium=input("indique si su pagara por cuenta premium=")
